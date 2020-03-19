426fadc1318264a32dac6f241848a54fcf79b4dd
Akhil Veeraghanta <akhilveeraghanta@AkhilsApleWatch.hitronhub.home>
2020-03-19 07:57:41 -0700

    Updates

---
+++ precache-manifest.8d5924c341d36f06759cb9dd07910073.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd8e7f02598f883b51025a82d32b0fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "580e14aac0879e537f4d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "df6ee10b0be385602ee4",
+    "url": "/static/js/2.4dae4fdf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4dae4fdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580e14aac0879e537f4d",
+    "url": "/static/js/main.084ccca6.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
